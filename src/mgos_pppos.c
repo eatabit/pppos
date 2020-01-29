02b895cfc05b4e44271b48542218fbc6de12eed0
Greg Oleksiak <greg.oleksiak@gmail.com>
2020-01-28 23:39:21 -0500

    creg = 2

--- src/mgos_pppos.c
+++ src/mgos_pppos.c
@@ -642,7 +642,7 @@ static void mgos_pppos_dispatch_once(struct mgos_pppos_data *pd) {
       add_cmd(pd, mgos_pppos_at_cb, "AT+COPS=3,0");
       add_cmd(pd, mgos_pppos_cops_cb, "AT+COPS?");
       add_cmd(pd, mgos_pppos_csq_cb, "AT+CSQ");
-      add_cmd(pd, NULL, "AT+CREG=0"); /* Disable unsolicited reports */
+      add_cmd(pd, NULL, "AT+CREG=2"); /* Enable unsolicited reports */
       add_cmd(pd, NULL, "AT+CGDCONT=1,\"IP\",\"%s\"", pd->cfg->apn);
       add_cmd(pd, mgos_pppos_atd_cb, "ATDT*99***1#");
       mgos_pppos_set_state(pd, PPPOS_CMD);
